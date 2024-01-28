9a71726ff9021cc2aa22fea0563c24d92c02e28c
wtchrs <wtchr_@hotmail.com>
2024-01-28 23:07:25 +0900

    Initial commit

---
+++ src/main/java/reserve/ReserveBackendApplication.java
@@ -0,0 +1,13 @@
+package reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/reserve/ReserveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
