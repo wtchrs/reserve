0102c098d9fc7c60adb3594a0d461047461f4989
wtchrs <wtchr_@hotmail.com>
2024-10-22 17:46:43 +0900

    frontend: Split the build result into multiple chunks

--- reserve-frontend/vite.config.ts
+++ reserve-frontend/vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-hook-form',
+            '@hookform/resolvers',
+            'zod'
+          ],
+          'mui': [
+            '@mui/material',
+            '@mui/icons-material',
+            '@emotion/react',
+            '@emotion/styled',
+            '@fontsource/roboto',
+          ],
+          'axios': ['axios'],
+        },
+      },
+    }
+  }
 })
