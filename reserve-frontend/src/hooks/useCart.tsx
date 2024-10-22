3b73f7fc1d368bffc2b26df4beb72304c6803c86
wtchrs <wtchr_@hotmail.com>
2024-10-22 21:24:36 +0900

    frontend: Persist cart data in local storage

--- reserve-frontend/src/hooks/useCart.tsx
+++ reserve-frontend/src/hooks/useCart.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useCallback, useContext, useState} from 'react'
+import {createContext, ReactNode, useCallback, useContext, useEffect, useState} from 'react'
 import type {CartItem, Menu, Store} from '@customTypes/domain.d.ts'
 
 type Cart = {
@@ -16,20 +16,39 @@ type CartContext = {
 
 const cartContext = createContext<CartContext>({} as CartContext)
 
+function getCartFromLocalStorage(): Cart {
+    const cart = localStorage.getItem('cart')
+    return cart ? JSON.parse(cart) : {items: []}
+}
+
+function setCartToLocalStorage(cart: Cart) {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
+
 export function CartProvider({children}: { children: ReactNode }) {
     const [cart, setCart] = useState<Cart>({items: []})
 
+    useEffect(() => {
+        setCart(getCartFromLocalStorage())
+    }, [])
+
+    // Update cart state and save to local storage
+    const updateCart = useCallback((newCart: Cart) => {
+        setCart(newCart)
+        setCartToLocalStorage(newCart)
+    }, [])
+
     const removeItem = useCallback((itemIndex: number) => {
-        const newCartItems = cart.items.slice()
-        newCartItems.splice(itemIndex, 1)
-        setCart({...cart, items: newCartItems})
-        if (newCartItems.length === 0) setCart({items: []})
-    }, [cart])
+        let newCart: Cart = {store: cart.store, items: cart.items.slice()}
+        newCart.items.splice(itemIndex, 1)
+        if (newCart.items.length === 0) newCart = {items: []}
+        updateCart(newCart)
+    }, [cart, updateCart])
 
     const addItem = useCallback((newStore: Store, menu: Menu, quantity: number) => {
         if (cart.store && cart.store.storeId !== newStore.storeId) {
             const newCartItems: CartItem[] = [{menuId: menu.menuId, name: menu.name, price: menu.price, quantity}]
-            setCart({store: newStore, items: newCartItems})
+            updateCart({store: newStore, items: newCartItems})
             return
         }
 
@@ -38,33 +57,33 @@ export function CartProvider({children}: { children: ReactNode }) {
         if (index !== -1 && quantity === 0) {
             removeItem(index)
             return
-        } else if (index !== -1) {
-            setCart({
-                store: newStore,
-                items: cart.items.map(item => item.menuId === menu.menuId ? {...item, quantity} : item),
-            })
-        } else {
-            setCart({
-                store: newStore,
-                items: [...cart.items, {menuId: menu.menuId, name: menu.name, price: menu.price, quantity}],
-            })
         }
-    }, [cart, removeItem])
+
+        const newCart = {store: newStore} as Cart
+        if (index !== -1) {
+            newCart.items = cart.items.map(item => item.menuId === menu.menuId ? {...item, quantity} : item)
+        } else {
+            newCart.items = [...cart.items, {menuId: menu.menuId, name: menu.name, price: menu.price, quantity}]
+        }
+        updateCart(newCart)
+    }, [cart, removeItem, updateCart])
 
     const updateItem = useCallback((itemIndex: number, quantity: number) => {
         if (quantity === 0) {
             removeItem(itemIndex)
             return
         }
-        setCart({
+        const newCart = {
             store: cart.store,
             items: cart.items.map((item, index) => index === itemIndex ? {...item, quantity} : item),
-        })
-    }, [cart, removeItem])
+        }
+        setCart(newCart)
+        updateCart(newCart)
+    }, [cart, removeItem, updateCart])
 
     const clear = useCallback(() => {
-        setCart({items: []})
-    }, [])
+        updateCart({items: []})
+    }, [updateCart])
 
     return (
         <cartContext.Provider value={{cart, addItem, updateItem, removeItem, clear}}>
